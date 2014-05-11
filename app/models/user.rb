2f35439eee8990d89fbfb3c8286dff22560499e2
antonLitovka <antlitovka@gmail.com>
2014-05-11 18:55:58 +0300

    test1

---
+++ app/models/user.rb
@@ -0,0 +1,31 @@
+class User < ActiveRecord::Base
+
+	validates :name, presence: true , length: { minimum: 6, maximum: 50 }
+	validates :email, presence: true, format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i},
+            uniqueness: {:case_sensitive => false}
+  validates :password, :length => {minimum: 6}
+
+  validates_confirmation_of :password,
+                            if: lambda { |m| m.password.present? }
+
+  before_save {self.email = email.downcase}
+  before_create :create_remember_token
+
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.encrypt(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  has_secure_password
+
+
+  private
+
+  def create_remember_token
+    self.remember_token = User.encrypt(User.new_remember_token)
+  end
+
+end
