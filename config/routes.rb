8c72ebcd7e4485432b3dc58ab3dd5f58b86c77ff
dmitrymov <dimmov21@gmail.com>
2014-05-31 16:48:53 +0300

    Commit

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ SampleApp::Application.routes.draw do
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
+  match '/showall', to: 'static_pages#showall', via: 'get'
 
 
   # The priority is based upon order of creation: first created -> highest priority.
