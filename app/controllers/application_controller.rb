812e8625f292347f4d22894e6d9e80c43c686589
dmitrymov <dimmov21@gmail.com>
2014-06-06 15:57:07 +0300

    Google sync deleted

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,12 +4,5 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
 
-  helper_method :current_calendar
-
-  private
-
-  def current_user
-    @current_calendar ||= Calendar.find(session[:calendar_id]) if session[:calendar_id]
-  end
 
 end
